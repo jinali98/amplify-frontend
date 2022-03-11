15683bd3130278f2ce1b3c513af0ff14f7334d2a
jinaliApium <jinali@apium.io>
2022-03-11 16:22:50 +0530

    updated commit

--- src/App.js
+++ src/App.js
@@ -14,7 +14,7 @@ function App() {
           method: "POST",
           headers: {
             "Access-Control-Allow-Headers": "Content-Type",
-            "Access-Control-Allow-Origin": "http://localhost:3000",
+            "Access-Control-Allow-Origin": "*",
             "Access-Control-Allow-Methods": "OPTIONS,POST,GET",
           },
           body: JSON.stringify({ todo }),
